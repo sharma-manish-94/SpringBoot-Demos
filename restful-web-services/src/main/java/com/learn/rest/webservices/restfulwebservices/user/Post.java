f97c7c15eeff6a56b6a50b7a795fccca49b6465a
code-manish <code.manish94@gmail.com>
2024-05-18 23:40:47 +0530

    added post and mapping between user and post

---
+++ restful-web-services/src/main/java/com/learn/rest/webservices/restfulwebservices/user/Post.java
@@ -0,0 +1,43 @@
+package com.learn.rest.webservices.restfulwebservices.user;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String description;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- restful-web-services/src/main/java/com/learn/rest/webservices/restfulwebservices/user/UserJpaResource.java
+++ restful-web-services/src/main/java/com/learn/rest/webservices/restfulwebservices/user/UserJpaResource.java
@@ -1,5 +1,6 @@
 package com.learn.rest.webservices.restfulwebservices.user;
 
+import com.learn.rest.webservices.restfulwebservices.exceptions.UserNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
@@ -15,29 +16,35 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserJpaResource {
-
-    private final UserDaoService userDaoService;
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserJpaResource(final UserDaoService userDaoService) {
-        this.userDaoService = userDaoService;
+    public UserJpaResource(final UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @GetMapping(path = "/jpa/users")
     public List<User> retrieveAllUsers() {
-        return userDaoService.findAll();
+//        return userDaoService.findAll();
+        return userRepository.findAll();
     }
 
     @GetMapping(path = "/jpa/users/{id}")
     public EntityModel<User> retrieveUserById(@PathVariable("id") final int id) {
-        User user = userDaoService.findUserById(id);
-        EntityModel<User> entityModel = EntityModel.of(user);
+//        User user = userDaoService.findUserById(id);
+        Optional<User> optionalUser = userRepository.findById(id);
+
+        if (optionalUser.isEmpty()) {
+            throw new UserNotFoundException("id: " + id);
+        }
+        EntityModel<User> entityModel = EntityModel.of(optionalUser.get());
         WebMvcLinkBuilder linkBuilder = linkTo(methodOn(this.getClass()).retrieveAllUsers());
         entityModel.add(linkBuilder.withRel("all-users"));
         return entityModel;
@@ -45,7 +52,8 @@ public class UserJpaResource {
 
     @PostMapping(path = "/jpa/users")
     public ResponseEntity<User> createUser(@Valid @RequestBody final User user) {
-        User newUser = userDaoService.createUser(user);
+//        User newUser = userDaoService.createUser(user);
+        User newUser = userRepository.save(user);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(newUser.getId()).toUri();
         MultiValueMap<String, String> headersMap = new HttpHeaders();
@@ -55,7 +63,8 @@ public class UserJpaResource {
 
     @DeleteMapping(path = "/jpa/users/{id}")
     public ResponseEntity<User> deleteUser(@PathVariable("id") final Integer id) {
-        userDaoService.deleteById(id);
+//        userDaoService.deleteById(id);
+        userRepository.deleteById(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
